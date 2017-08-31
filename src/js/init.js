5fce059564fe4ddba29b815bcf1f9400f8ef407a
mefistofelus <manilo@ukr.net>
2017-09-01 02:26:13 +0300

    доделал все, кроме футера и сайдбаров на странице категорий

---
+++ src/js/init.js
@@ -0,0 +1,15 @@
+$( function() {
+    $( "#slider-range" ).slider({
+        range: true,
+        min: 1,
+        max: 2000,
+        values: [ 100, 1000 ],
+        slide: function( event, ui ) {
+            $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+        }
+    });
+
+    $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
+        " - $" + $( "#slider-range" ).slider( "values", 1 ) );
+
+} );
