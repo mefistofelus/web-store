673c91d9c71195c5e7727e0a20ba6e0fc74032f5
Vasiliy <manilo@ukr.net>
2017-09-19 17:05:12 +0300

    дз почти готово

--- src/js/plugin.js
+++ src/js/plugin.js
@@ -1,15 +1,14 @@
-$( function() {
+$( function () {
 
     var sidebar_btn = $('.mobile-sidebar-btn');
     var main = $('main');
     var overlay = $('.overlay');
-
+    
     sidebar_btn.on('click', function (e) {
         main.toggleClass('show');
     });
-
     overlay.on('click', function (e) {
         main.toggleClass('show');
-    });
+    })
 
 });
